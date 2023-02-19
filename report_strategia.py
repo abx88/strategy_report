3050b6b11d99823f84f633a6e32d03094d4a886e
abx88 <a.bucalossi88@gmail.com>
2023-02-19 13:44:11 +0100

    Update report_strategia.py

--- report_strategia.py
+++ report_strategia.py
@@ -296,15 +296,14 @@ elif pagina=='Montecarlo':
         #matrix_of_equities.to_csv('matrix_of_equities.csv', sep=',', decimal='.', index=False)
         #matrix_of_drawdowns.to_csv('matrix_of_drawdowns.csv', sep=',', decimal='.', index=False)
 
-        worst_drawdown = round(matrix_of_drawdowns.min().min(),2)
+        worst_drawdown = round(matrix_of_drawdowns.min().min(), 2)
         worst_drawdown_index = matrix_of_drawdowns.min(axis=1).idxmin()
-        worst_drawdown_profit = round(matrix_of_equities[worst_drawdown_index]\
-                                                        [matrix_of_equities[worst_drawdown_index].count()-1],2)
+        worst_drawdown_profit = round(matrix_of_equities.iloc[-1, worst_drawdown_index], 2)
 
-        best_drawdown = round(matrix_of_drawdowns.min().max(),2)
+        best_drawdown = round(matrix_of_drawdowns.min().max(), 2)
         best_drawdown_index = matrix_of_drawdowns.min(axis=1).idxmax()
-        best_drawdown_profit = round(matrix_of_equities[best_drawdown_index]\
-                                                       [matrix_of_equities[best_drawdown_index].count()-1],2)
+        best_drawdown_profit = round(matrix_of_equities.iloc[-1, best_drawdown_index], 2)
+
 
         st.text("Original Profit/Loss: "+str(original_profit))
         st.text("Original Max Draw Down: "+ str(original_max_drawdown))
