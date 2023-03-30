b6cbf212adee8eabbea9d3b3995bcdf763da8018
abx88 <a.bucalossi88@gmail.com>
2023-03-30 13:07:55 +0200

    Update report_strategia.py

--- report_strategia.py
+++ report_strategia.py
@@ -211,7 +211,8 @@ if pagina=='Riepilogo_equity':
     if len(uploaded_files) == 0:
             st.text("nessun dato")
     else:
-        operazioni=sum(np.where(((dfriep.type=='buy')|(dfriep.type=='sell')),1,0))
+        #operazioni=sum(np.where(((dfriep.type=='buy')|(dfriep.type=='sell')),1,0))
+        operazioni= len(dfriep)
         st.text("Profitto netto totale: "+str(bt.profit(dfriep.cumulativeGLOB)))
         st.text("ROI su capitale iniziale: "+str(round((bt.profit(dfriep.cumulativeGLOB)/initial_capital*100),2))+'%')
         st.text("operazioni: " + str(operazioni))
